06c489bee7e72a7dfd70a99bd43b5fe8bba2d6c7
Eurico Nicacio <euriconicaciojr@gmail.com>
2021-09-21 21:55:33 -0300

    Aula 2

--- aula1-mod4/src/messages/messages.controller.ts
+++ aula1-mod4/src/messages/messages.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { NotFoundError } from 'rxjs';
+import { Message } from './Message';
+import { MessageDto } from './MessageDto';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
@@ -12,12 +15,25 @@ export class MessagesController {
 
     @Get(':id')
     getById(@Param() params){
-        return this.messagesService.findById(+params.id);
+        return this.messagesService.findById(+params.id).catch((e) => {
+            throw new NotFoundException(e.message);
+        });
     }
 
     @Post()
-    create(@Body() body: any){
-        return this.messagesService.create(body);
+    create(@Body() messageDto: MessageDto){
+        return this.messagesService.create(messageDto);
     }
+
+    @Put(':id')
+    update(@Param() params, @Body() messageDto: MessageDto){
+        return this.messagesService.update(+params.id, messageDto);
+    }
+
+    @Delete(':id')
+    delete(@Param() params){
+        return this.messagesService.delete(+params.id);
+    }
+
 
 }
--- aula1-mod4/src/messages/messages.service.ts
+++ aula1-mod4/src/messages/messages.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Message } from './Message';
+import { MessageDto } from './MessageDto';
 
 @Injectable()
 export class MessagesService {
-
-    public messages = [
+    public messages: Message[] = [
         {
             id: 1,
             texto: "Primeira mensagem"
@@ -18,19 +19,56 @@ export class MessagesService {
         },
         {
             id: 4,
-            texto: "Quarta mensagem"
+            texto: "Quarta mensagem",
         },
     ];
 
     findAll(){
-        return this.messages;
+        return this.messages.filter(Boolean);
     }
     
-    findById(id: number) {
-        return this.messages.find(message => message.id === id);
+    async findById(id: number) {
+        const message = this.messages.find(message => message.id === id);
+
+        if(!message){
+            throw Error(`Mensagem com id ${id} não encontrada`);
+        }
+
+        return message;
     }
     
-    create(message: {id: number, texto: string}) {
+    create(messageDto: MessageDto) {
+        const id = this.messages.length + 1;
+
+        const message : Message = {
+            id,
+            ...messageDto,
+        }
+        
         this.messages.push(message);
+
+        return message;
+    }
+
+    update(id:number, messageDto: MessageDto) {
+        const index = this.messages.findIndex(a => a.id === id);
+
+        const message : Message = {
+            id,
+            ...messageDto,
+        }
+
+        this.messages[index] = message;
+
+        return message;
+    }
+    
+    delete(id: number) {
+        const index = this.messages.findIndex(a => a.id === id);
+
+        delete this.messages[index];
+
+        return true;
+
     }
 }
